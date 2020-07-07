68a0a2608dfa03b523b1a71bfbad02fb5da78c38
JeromeAmbroise <jerome.ambroise@uclouvain.be>
2020-07-07 10:53:40 +0200

    add function extract.phylum and extract.bacteria.table

--- R/functions.R
+++ R/functions.R
@@ -25,3 +25,31 @@ compute.n.bacteria <- function()
   bacteria.n <- data.frame(proks.selected%>%group_by(Organism)%>%summarise(n=n()))
   return(bacteria.n)
 }
+
+
+
+extract.phylum <- function()
+{
+  library(taxize)
+  library(dplyr)
+  phylum <- itis_downstream(id = 50, downto="phylum")
+  phylum <- tibble(phylum)
+  phylum <- phylum %>% select(tsn,taxonname)
+  return(phylum)
+}
+
+
+extract.bacteria.table <- function()
+{
+  library(genomes)
+  library(dplyr)
+  proks <- reports("prokaryotes.txt")
+  proks.selected <- proks%>%filter(Status=='Complete Genome'|Status=='Chromosome')
+  proks.selected <- proks.selected[grep(paste(phylum$taxonname,collapse='|'),proks.selected$SubGroup,ignore.case = T),]
+  proks.selected$Organism <- unlist(lapply(strsplit(proks.selected$Organism,split=' '),function(x) paste(x[1],x[2],sep=' ')))
+  proks.selected$Organism <- gsub(proks.selected$Organism,pattern='\\[',replacement = '')
+  proks.selected$Organism <- gsub(proks.selected$Organism,pattern='\\]',replacement = '.')
+  proks.selected$Organism <- gsub(proks.selected$Organism,pattern='\'',replacement = '')
+  proks.selected$Organism <- proks.selected$Organism %>% gsub(' ','_',.)
+  return(proks.selected)
+}
