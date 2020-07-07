6cd96648c12d6408aa318a7c04ed63060dc18550
Maxime Bonjean <m.bonjean@student.uclouvain.be>
2020-07-07 14:17:50 +0200

    initiation download-genome

---
+++ R/download-genome.R
@@ -0,0 +1,34 @@
+downloadgenome <- function(maxOrganism,accession.list,repertoire)
+{
+  library(reutils)
+  library(ape)
+  library(seqinr)
+  library(Biostrings)
+  library(dplyr)
+  library(WriteXLS)
+  species <- gsub(basename(accession.list),pattern = '.csv',replacement = '')
+  n.species <- length(species)
+  for(i in 1:n.species)
+  {
+    dir.create(paste0(repertoire,species[i]))
+    accession <- read.csv(accession.list[i],stringsAsFactors = F)
+    accession  <- accession$accession
+    N.accession <- length(accession)
+    print(N.accession)
+    for(j in 1:min(c(maxOrganism,N.accession)))
+    {
+      current.accession <- accession[j]
+      current.accession <- strsplit(current.accession,split=':')[[1]]
+      current.accession <- gsub(current.accession,pattern = ' ',replacement = '')
+      N.chromosomes <- length(current.accession)
+      dir.create(paste0(repertoire,gsub(species[i],pattern = ' ',replacement = '.'),'/',current.accession[1]))
+      for(k in 1:min(5,N.chromosomes))
+      {
+        seq <- read.GenBank(current.accession[k])
+        write.FASTA(seq,paste0(repertoire,gsub(species[i],pattern = ' ',replacement = '.'),'/',current.accession[1],'/',current.accession[k],'.fasta'))
+      }
+      seq <- readDNAStringSet(list.files(paste0(repertoire,species[i],'/',current.accession[1]),full.names = T))
+      writeXStringSet(seq,paste0(repertoire,gsub(species[i],pattern = ' ',replacement = '.'),'/',paste(current.accession[1:min(5,N.chromosomes)],collapse = ':'),'.fasta'))
+    }
+  }
+}
