b4c6b1422684cf5f11698be6294e43604d18ad0e
Maxime Bonjean <m.bonjean@student.uclouvain.be>
2020-07-08 13:19:26 +0200

    petit changement

--- R/extract.bacteria.table.R
+++ R/extract.bacteria.table.R
@@ -10,7 +10,7 @@ extract.bacteria.table <- function(phylum)
   library(genomes)
   library(dplyr)
   proks <- reports("prokaryotes.txt")
-  proks.selected <- proks%>%filter(Status=='Complete Genome'|Status=='Chromosome')
+  bacteria.table <- proks%>%filter(Status=='Complete Genome'|Status=='Chromosome')
   bacteria.table <- bacteria.table[grep(paste(phylum$taxonname,collapse='|'),bacteria.table$SubGroup,ignore.case = T),]
   bacteria.table$Organism <- unlist(lapply(strsplit(bacteria.table$Organism,split=' '),function(x) paste(x[1],x[2],sep=' ')))
   bacteria.table$Organism <- gsub(bacteria.table$Organism,pattern='\\[',replacement = '')
